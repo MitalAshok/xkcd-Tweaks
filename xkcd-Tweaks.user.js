534486db0f427c4ed297bd673eae86a736a9fccb
MitalAshok <mital.vaja@googlemail.com>
2017-05-09 13:17:26 +0100

    Minor style fixes and accounted for fixed /1663/

--- xkcd-Tweaks.user.js
+++ xkcd-Tweaks.user.js
@@ -3,14 +3,14 @@
 // @description   Some tweaks to xkcd.com and what-if.xkcd.com
 // @include       /^(https?:)?(\/\/)?(www\.)?(what-?if\.)?xkcd\.com/
 // @author        Mital Ashok
-// @icon          data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABX0lEQVRYhcWVMZIEIQhFOYrhHM8baGjoEbyRYYcehY2cchXwt7O9Q1VXzQDKAxSJiPjLH/G3xARwzi20zrnnAaTAT4GIAGjvJAkhLH4hBBxgzl5ZIFbBqpxWsQXAyk4CQYJbEH8GcNo2EQAVC2CU1to9gE8rIJW6t2gLoDlKC8dAyPWEAOZslAWLDYWGAE5ONBFxa00N3s8BBGCBbO6z2f/bAPPmiM/H11AjtzL03sMA3nsM4M5YfQRgLJn2WxK09CZAz75nOm4y206rpgJIAeYsJJ9T6AVAGzKW7hR6AZDKNZZV0xsb/9IZoPIB211DLbgGPdpEgFlmoN2JZmautS7QtVbVfzsHXq/XorNevJQSExGXUriUwkTEKaX7AJ3+ui5Rv9mQY4wcYzybA1YgFGD+r7UBGkSorWecc37rcs5mG0SAu1nO+nHej+/EvwGgehFgvD47AHQdYMOe0qe+Hzm8hknb8QYpAAAAAElFTkSuQmCC
+// @icon          https://xkcd.com/favicon.ico
 // @namespace     https://greasyfork.org/en/users/59570-mital-ashok
 // @license       MIT License
-// @version       3.0.3
+// @version       3.0.4
 // @grant         none
 // @updateurl     https://mitalashok.github.io/xkcd-Tweaks.user.js
-// @noframes
 // @run-at        document-start
+// @noframes
 // ==/UserScript==
 
 // @require       https://raw.githubusercontent.com/stefanpenner/es6-promise/master/dist/es6-promise.auto.min.js
@@ -70,10 +70,10 @@
     }
 
     function xhr(url, timeout, method) {
-      return new window.Promise(function (resolve, reject) {
+      return new window.Promise(function(resolve, reject) {
         var resolved = false;
         var x = new window.XMLHttpRequest();
-        x.onreadystatechange = function () {
+        x.onreadystatechange = function() {
           if (x.readyState === window.XMLHttpRequest.DONE) {
             resolve(x.responseText, x);
             resolved = true;
@@ -125,7 +125,7 @@
       number = +head_anchor.getAttribute('href').slice(19, -1);
 
       if (/^(https?:)?(\/\/)?(www\.)?what-if\.xkcd\.com\/\d+/i.test(window.location.href)) {
-        repeat(100, function () {
+        repeat(100, function() {
           head_anchor.removeAttribute('href');
           head_anchor.style.textDecoration = 'underline';
           return !head_anchor.hasAttribute('href') && head_anchor.style.textDecoration === 'underline';
@@ -215,7 +215,7 @@
           return;
           case key_codes.left: case key_codes.p:
           if (is_last) return;
-          window.Array.prototype.forEach.call(document.getElementsByTagName('a'), function (a) {
+          window.Array.prototype.forEach.call(document.getElementsByTagName('a'), function(a) {
             if (a.firstChild !== null && a.firstChild.textContent === 'Prev') {
               a.click();
             }
@@ -230,7 +230,7 @@
               window.location.replace(window.location.origin + '/' + random + '/');
             });
         }
-      });
+      }, true);
       window.Array.prototype.forEach.call(document.getElementsByTagName('img'), function(img) {
         if (img.hasAttribute('title')) {
           if (border) {
@@ -253,12 +253,35 @@
       });
       return;
     }
-    if (window.location.pathname === '/404/' || window.location.pathname === '/404') {
-      var slash = window.location.pathname === '/404' ? '/' : '';
-      document.documentElement.innerHTML =
-        "<!DOCTYPE html>\n<html>\n<head>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"/s/b0dcca.css\" title=\"Default\"/>\n<title>xkcd: Not Found</title>\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"/>\n<link rel=\"shortcut icon\" href=\"/s/919f27.ico\" type=\"image/x-icon\"/>\n<link rel=\"icon\" href=\"/s/919f27.ico\" type=\"image/x-icon\"/>\n<link rel=\"alternate\" type=\"application/atom+xml\" title=\"Atom 1.0\" href=\"/atom.xml\"/>\n<link rel=\"alternate\" type=\"application/rss+xml\" title=\"RSS 2.0\" href=\"/rss.xml\"/>\n<script>\n(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\nm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n\nga('create', 'UA-25700708-7', 'auto');\nga('send', 'pageview');\n</script>\n<script type=\"text/javascript\" src=\"//xkcd.com/1350/jquery.min.js\"></script>\n<script type=\"text/javascript\" src=\"//xkcd.com/static/json2.js\"></script>\n\n</head>\n<body>\n<div id=\"topContainer\">\n<div id=\"topLeft\">\n<ul>\n<li><a href=\"/archive\">Archive</a></li>\n<li><a href=\"http://what-if.xkcd.com\">What If?</a></li>\n<li><a href=\"http://blag.xkcd.com\">Blag</a></li>\n<li><a href=\"http://store.xkcd.com/\">Store</a></li>\n<li><a rel=\"author\" href=\"/about\">About</a></li>\n</ul>\n</div>\n<div id=\"topRight\">\n<div id=\"masthead\">\n<span><a href=\"/\"><img src=\"//imgs.xkcd.com/static/terrible_small_logo.png\" alt=\"xkcd.com logo\" height=\"83\" width=\"185\"/></a></span>\n<span id=\"slogan\">A webcomic of romance,<br/> sarcasm, math, and language.</span>\n</div>\n<div id=\"news\">\nxkcd updates every Monday, Wednesday, and Friday.\n<script>\nmu = window.location.href.split(\"#\")[1]\nif(/([0-9a-b]+-)[4][0-9a-b]+/.test(mu)) { window.location.href = \"http://xkcd.com/1663/#\"+mu }\n</script>\n\n\n</div>\n</div>\n<div id=\"bgLeft\" class=\"bg box\"></div>\n<div id=\"bgRight\" class=\"bg box\"></div>\n</div>\n<div id=\"middleContainer\" class=\"box\">\n\n<div id=\"ctitle\">Not Found</div>\n<ul class=\"comicNav\">\n<li><a href=\"/1/\">|&lt;</a></li>\n<li><a rel=\"prev\" href=\"/403/\" accesskey=\"p\">&lt; Prev</a></li>\n<li><a href=\"//c.xkcd.com/random/comic/\">Random</a></li>\n<li><a rel=\"next\" href=\"/405/\" accesskey=\"n\">Next &gt;</a></li>\n<li><a href=\"/\">&gt;|</a></li>\n</ul>\n<div id=\"comic\">\n<iframe title=\"April Fools! (This page was created by xkcd tweaks. It would normally be a HTTP 404 error page.)\" style=\"border: 1px solid gray; outline: 1px solid black; margin-top: 1px; width: 296px; height: 296px;\" src=\"/404" + slash + "\" height=\"296\" width=\"296\"><h1 style=\"text-align: left; font-size: 2em; font-variant: normal; font-family: Times New Roman; font-weight: bold;\">404 - Not Found</h1></iframe><div style=\"width: 300px; height: 300px; position: absolute; left: 240px; margin-top: -299px;\" title=\"April Fools! (This page was created by xkcd tweaks. It would normally be a HTTP 404 error page.)\"></div>\n</div>\n<ul class=\"comicNav\">\n<li><a href=\"/1/\">|&lt;</a></li>\n<li><a rel=\"prev\" href=\"/403/\" accesskey=\"p\">&lt; Prev</a></li>\n<li><a href=\"//c.xkcd.com/random/comic/\">Random</a></li>\n<li><a rel=\"next\" href=\"/405/\" accesskey=\"n\">Next &gt;</a></li>\n<li><a href=\"/\">&gt;|</a></li>\n</ul>\n<br />\nPermanent link to this comic: http://xkcd.com/404/<br />\nImage URL (for hotlinking/embedding): http://imgs.xkcd.com/comics/\n<div id=\"transcript\" style=\"display: none\">[[A couple sit at the small table of a cafe.  The woman holds up a graph.]]\nWoman: We&#39;re a terrible match.  But if we sleep together, it&#39;ll make the local hookup network a symmetric graph.\nMan: I can&#39;t argue with that.\n{{Title text: Check it out; I&#39;ve had sex with someone who&#39;s had sex with someone who&#39;s written a paper with Paul Erd\xC5\x91s!}}</div>\n</div>\n<div id=\"bottom\" class=\"box\">\n<img src=\"//imgs.xkcd.com/s/a899e84.jpg\" width=\"520\" height=\"100\" alt=\"Selected Comics\" usemap=\"#comicmap\"/>\n<map id=\"comicmap\" name=\"comicmap\">\n<area shape=\"rect\" coords=\"0,0,100,100\" href=\"/150/\" alt=\"Grownups\"/>\n<area shape=\"rect\" coords=\"104,0,204,100\" href=\"/730/\" alt=\"Circuit Diagram\"/>\n<area shape=\"rect\" coords=\"208,0,308,100\" href=\"/162/\" alt=\"Angular Momentum\"/>\n<area shape=\"rect\" coords=\"312,0,412,100\" href=\"/688/\" alt=\"Self-Description\"/>\n<area shape=\"rect\" coords=\"416,0,520,100\" href=\"/556/\" alt=\"Alternative Energy Revolution\"/>\n</map>\n<div>\n<!--\nSearch comic titles and transcripts:\n<script type=\"text/javascript\" src=\"//www.google.com/jsapi\"></script>\n<script type=\"text/javascript\">google.load('search', '1');google.setOnLoadCallback(function() {google.search.CustomSearchControl.attachAutoCompletion('012652707207066138651:zudjtuwe28q',document.getElementById('q'),'cse-search-box');});</script>\n<form action=\"//www.google.com/cse\" id=\"cse-search-box\">\n<div>\n<input type=\"hidden\" name=\"cx\" value=\"012652707207066138651:zudjtuwe28q\"/>\n<input type=\"hidden\" name=\"ie\" value=\"UTF-8\"/>\n<input type=\"text\" name=\"q\" id=\"q\" size=\"31\"/>\n<input type=\"submit\" name=\"sa\" value=\"Search\"/>\n</div>\n</form>\n<script type=\"text/javascript\" src=\"//www.google.com/cse/brand?form=cse-search-box&amp;lang=en\"></script>\n-->\n<a href=\"/rss.xml\">RSS Feed</a> - <a href=\"/atom.xml\">Atom Feed</a>\n</div>\n<br />\n<div id=\"comicLinks\">\nComics I enjoy:<br/>\n        <a href=\"http://threewordphrase.com/\">Three Word Phrase</a>,\n        <a href=\"http://www.smbc-comics.com/\">SMBC</a>,\n        <a href=\"http://www.qwantz.com\">Dinosaur Comics</a>,\n        <a href=\"http://oglaf.com/\">Oglaf</a> (nsfw),\n        <a href=\"http://www.asofterworld.com\">A Softer World</a>,\n        <a href=\"http://buttersafe.com/\">Buttersafe</a>,\n        <a href=\"http://pbfcomics.com/\">Perry Bible Fellowship</a>,\n        <a href=\"http://questionablecontent.net/\">Questionable Content</a>,\n        <a href=\"http://www.buttercupfestival.com/\">Buttercup Festival</a>,\n        <a href=\"http://www.mspaintadventures.com/?s=6&p=001901\">Homestuck</a>,\n\t<a href=\"http://www.jspowerhour.com/\">Junior Scientist Power Hour</a>\n</div>\n<p>Warning: this comic occasionally contains strong language (which may be unsuitable for children), unusual humor (which may be unsuitable for adults), and advanced mathematics (which may be unsuitable for liberal-arts majors).</p>\n<div id=\"footnote\">BTC 1FhCLQK2ZXtCUQDtG98p6fVH7S6mxAsEey<br />We did not invent the algorithm. The algorithm consistently finds Jesus. The algorithm killed Jeeves. <br/>The algorithm is banned in China. The algorithm is from Jersey. The algorithm constantly finds Jesus.<br/>This is not the algorithm. This is close.</div>\n<div id=\"licenseText\">\n<p>\nThis work is licensed under a\n<a href=\"http://creativecommons.org/licenses/by-nc/2.5/\">Creative Commons Attribution-NonCommercial 2.5 License</a>.\n</p><p>\nThis means you're free to copy and share these comics (but not to sell them). <a rel=\"license\" href=\"/license.html\">More details</a>.</p>\n</div>\n</div>\n</body>\n<!-- Layout by Ian Clasbey, davean, and chromakode -->\n</html>\n\n";
-    }
     var comic = document.getElementById('comic');
+    if ((window.location.pathname === '/404/' || window.location.pathname === '/404') && !comic) {
+      var slash = window.location.pathname === '/404' ? '/' : '';
+      xhr('/403/').then(function(page) {
+        var title_text = 'April Fools! (This page was created by xkcd tweaks. It would normally be a HTTP 404 error page.)';
+        var title = 'Not Found';
+        document.documentElement.innerHTML = page;
+        document.title = 'xkcd: ' + title;
+        document.getElementById('comic').innerHTML =
+          '<iframe title="' + title_text + '" style="border:1px solid gray;outline:1px solid black;margin-top:1px;width:296px;height:296px;" src="/404' + slash + '" height="296" width="296">' +
+              '<h1 style="text-align:left;font-size:2em;font-variant:normal;font-family:Times New Roman;font-weight:bold;">404 - Not Found</h1>' +
+          '</iframe>' +
+          '<div style="width:300px;height:300px;position:absolute;left:240px;margin-top:-299px;" title="' + title_text + '"></div>';
+        document.getElementById('ctitle').textContent = title;
+        var img_link = document.getElementById('transcript').previousSibling;
+        var perma_link = img_link.previousSibling.previousSibling;
+        img_link.textContent = img_link.textContent.replace('convincing_pickup_line.png', '');
+        perma_link.textContent = perma_link.textContent.replace('403', '404');
+        window.Array.prototype.forEach.call(document.querySelectorAll('a[rel="prev"]'), function(el) {
+          el.setAttribute('href', '/403/');
+        });
+        window.Array.prototype.forEach.call(document.querySelectorAll('a[rel="next"]'), function(el) {
+          el.setAttribute('href', '/405/');
+        });
+        main_run = false;
+        main();
+      });
+      return;
+    }
     if (!comic) {
       return;
     }
@@ -266,7 +289,13 @@
       var get_top = function() {
         return ((document.getElementById('middleContainer') || {}).offsetTop || 173) + 6;
       };
-      window.scrollTo(0, get_top());
+      window.setTimeout(function() {
+        window.scrollTo({
+          left: 0,
+          top: get_top(),
+          behavior: 'smooth'
+        });
+      }, 500);
       var scroll_interval_id = null;
       window.addEventListener('scroll', function() {
         var scroll_position = document.documentElement.scrollTop || document.body.scrollTop;
@@ -275,7 +304,11 @@
           if (scroll_interval_id === null && scroll_position !== top) {
             scroll_interval_id = window.setInterval(function() {
               if (scroll_position > top - 145 && top + 10 > scroll_position) {
-                window.scrollTo(0, top);
+                window.scrollTo({
+                  left: 0,
+                  top: top,
+                  behavior: 'smooth'
+                });
               }
               window.clearInterval(scroll_interval_id);
               scroll_interval_id = null;
@@ -287,7 +320,7 @@
             scroll_interval_id = null;
           }
         }
-      });
+      }, false);
     }
 
     var img_link = document.getElementById('transcript').previousSibling;
@@ -312,7 +345,7 @@
 
     var next_buttons;
     var previous_buttons;
-    repeat(100, function () {
+    repeat(100, function() {
       next_buttons = window.Array.prototype.slice.call(document.querySelectorAll('[rel="next"], [href="/"]'), 1);
       previous_buttons = document.querySelectorAll('[rel="prev"], [href="/1/"]');
       return next_buttons.length === 4 && previous_buttons.length === 4;
@@ -341,7 +374,7 @@
       }
     });
 
-    repeat(1000, function () {
+    repeat(1000, function() {
       if (comic.firstElementChild.tagName.toLowerCase() === 'script') {
         return false;
       }
@@ -398,7 +431,7 @@
         return;
       }
       var e = event || window.event || {};
-      var key_code = event.which || event.charCode || event.keyCode;
+      var key_code = e.which || e.charCode || e.keyCode;
       if (number === 1608 && document.activeElement.id === 'explore' && (key_code === key_codes.right || key_code === key_codes.left)) {
         return;
       }
@@ -413,7 +446,7 @@
       if (node) {
         node.click();
       }
-    });
+    }, true);
     if (transcript) {
       var info;
       if (1662 >= number && number >= 1609) {
@@ -424,7 +457,7 @@
         info = 'info.0.json';
       }
 
-      var others/* = new window.RegExp('\\s*(?:' + [
+      var others = new window.RegExp('\\s*(?:' + [
         "{{It's commonly known that too much perspective can be a downer.}}",
         '{{They are six-legged spiders}}',
         "{{I don't want to talk about it}}",
@@ -443,9 +476,7 @@
       ].map(
         function(s) {
           return s.replace(/[.^$*+?()[{\\|\-\]]/g, (function(m) { return '\\' + m; }));
-        }).join('|') + ')\\s*$')*/;
-      others = /\s*(?:\{\{It's commonly known that too much perspective can be a downer\.}}|\{\{They are six\-legged spiders}}|\{\{I don't want to talk about it}}|\{\{No more, no less}}|\{\{Love and circuit analysis, hand in hand at last\.}}|\{\{Medium: Pencil on paper}}|\{\{Mouseover text: "\.\.\."}}|\{\{It's science!}}|\{\{alt: I always wanted to impress them with how well I could hear, didn't you\? Also, this sets the record for number of awkward\-pause panels in one strip \(previously held by Achewood\)\]\]|\{\{Your IDE's color may vary\.}}|alt\-text: And she's gonna feel like a jerk when she realizes it was actually Under Pressure\.|\{\{alt text: Also, I hear the 4th root of \(9\^2 \+ 19\^2\n22\) is pi\.|\{\[title text: A tribute to Buttercup Festival\.}}|\[\[Alt text: If you're interested in the subject, Lawrence Lessig's 'Free Culture' is pretty good\]\]|\{\{Mouseover text:  James suggested this, and I'd have to agree\.  It'd be much worse\.}})\s*$/;
-      // `others` compiled from above.
+        }).join('|') + ')\\s*$');
 
       xhr(info).then(function(i) {
         var response;
@@ -485,18 +516,18 @@
     return;
   }
 
-  var new_location = window.location.href.toLowerCase()
-    .replace(/^(?:http(s?):)?(?:\/\/)?(?:www\.)?/, 'http$1://')
+  var location = window.location.href;
+  var new_location = location.toLowerCase()
+    .replace(/^(?:https?:)?(?:\/\/)?(?:www\.)?/, 'https://')
     .replace(/^https?:\/\/whatif/, 'https://what-if')
-    .replace(/^https:\/\/xkcd\.com\/1663/, 'http://xkcd.com/1663')
     .replace(/\/#$/, '/')
-    .replace(/^(https:\/\/(what-if\.)?xkcd\.com\/\d+)$/, '$1/');
-  if (new_location !== window.location.href) {
+    .replace(/^(https:\/\/(?:what-if\.)?xkcd\.com\/\d+)$/, '$1/');
+  if (new_location !== location) {
     window.location.replace(new_location);
   }
 
-  document.addEventListener('DOMContentLoaded', main);
-  window.addEventListener('load', main);
+  document.addEventListener('DOMContentLoaded', main, true);
+  window.addEventListener('load', main, true);
   if (document.readyState !== 'loading') {
     main();
   }
